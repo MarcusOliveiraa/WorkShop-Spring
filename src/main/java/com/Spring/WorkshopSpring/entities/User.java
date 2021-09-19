1fe097abb61b68ddaf00623abd41fad021d59e63
Marcus <marcus_oliveira1@protonmail.com>
2021-09-19 20:52:56 -0300

    Banco de dados H2, test profile, JPA

--- src/main/java/com/Spring/WorkshopSpring/entities/User.java
+++ src/main/java/com/Spring/WorkshopSpring/entities/User.java
@@ -1,14 +1,21 @@
 package com.Spring.WorkshopSpring.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
